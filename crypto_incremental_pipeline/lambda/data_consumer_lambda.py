6f1a2a5c0154671d27dd55b0a8b8aad480cf051b
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2023-12-29 11:41:21 -0600

    Update data_consumer_lambda.py

--- crypto_incremental_pipeline/lambda/data_consumer_lambda.py
+++ crypto_incremental_pipeline/lambda/data_consumer_lambda.py
@@ -10,7 +10,7 @@ TABLE_NAME = os.environ["DYNAMO_TABLE_NAME"]
 def handler(event, context):
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(TABLE_NAME)
-
+ 
     for record in event["Records"]:
         payload = base64.b64decode(record["kinesis"]["data"])
         data = json.loads(payload.decode("utf-8"), parse_float=Decimal)
